ee42764070fdfec93ee1f3df08b13e42af9e3051
Eke Chinedu <ekechinedu488@gmail.com>
2020-10-12 16:45:55 +0100

    second still

---
+++ src/app/product.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+import { Http, Response } from '@angular/http';
+
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ProductService {
+
+    private_albumUrl = '../assets/album.json';
+
+    constructor(private_Http: Http) {}
+
+    getAlbum{id: number} {
+        return this._http.get(this._albumUrl).map((reponse) => 
+        this.response.json());
+    }
+}
